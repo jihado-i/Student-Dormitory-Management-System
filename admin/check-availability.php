010d96acd46af5c1c606795f67ee8bf82544bc32
Muhammed-Adra <121480694+Muhammed-Adra@users.noreply.github.com>
2023-06-02 20:58:02 +0300

    Add files via upload

---
+++ admin/check-availability.php
@@ -0,0 +1,53 @@
+<?php
+    require_once("../includes/dbconn.php");
+    if(!empty($_POST["emailid"])) {
+        $email= $_POST["emailid"];
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)===false) {
+
+            echo "error : Geçerli bir e-posta girmediniz.";
+        } else {
+            $result ="SELECT count(*) FROM userRegistration WHERE email=?";
+            $stmt = $mysqli->prepare($result);
+            $stmt->bind_param('s',$email);
+            $stmt->execute();
+    $stmt->bind_result($count);
+    $stmt->fetch();
+    $stmt->close();
+    if($count>0){
+    echo "<span style='color:red'> Bu e-posta zaten var.</span>";
+        } else {
+            echo "<span style='color:green'> E-posta kayıt için kullanılabilir.</span>";
+        }
+     }
+    }
+
+    if(!empty($_POST["oldpassword"])) {
+    $pass=$_POST["oldpassword"];
+    $result ="SELECT password FROM userregistration WHERE password=?";
+    $stmt = $mysqli->prepare($result);
+    $stmt->bind_param('s',$pass);
+    $stmt->execute();
+    $stmt -> bind_result($result);
+    $stmt -> fetch();
+    $opass=$result;
+    if($opass==$pass) 
+    echo "<span style='color:green'> Parola eşleşiyor.</span>";
+    else echo "<span style='color:red'>şifre eşleşmiyor!</span>";
+    }
+
+
+    if(!empty($_POST["roomno"])) {
+    $roomno=$_POST["roomno"];
+    $result ="SELECT count(*) FROM registration WHERE roomno=?";
+    $stmt = $mysqli->prepare($result);
+    $stmt->bind_param('i',$roomno);
+    $stmt->execute();
+    $stmt->bind_result($count);
+    $stmt->fetch();
+    $stmt->close();
+    if($count>0)
+    echo "<span style='color:red'>$count. Oda önceden rezerve edilmiştir.</span>";
+    else
+        echo "<span style='color:red'>oda müsait</span>";
+    }
+?>
